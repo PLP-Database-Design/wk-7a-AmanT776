a4cab10144555c7e831821661ca7b997c9ec2208
Amanuel Tesfaye <amantes776@gmail.com>
2025-05-10 16:59:37 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -8,3 +8,16 @@ FROM
 CROSS APPLY 
     STRING_SPLIT(Products, ',');
 --QUESTION 2--
+--SPLIT STEP 1: CREATE ORDERS TABLE
+SELECT DISTINCT
+    OrderID,
+    CustomerName
+FROM 
+    OrderDetails;
+--SPLIT STEP2: CREATE ORDERITEMS TABLE
+SELECT 
+    OrderID,
+    Product,
+    Quantity
+FROM 
+    OrderDetails;
